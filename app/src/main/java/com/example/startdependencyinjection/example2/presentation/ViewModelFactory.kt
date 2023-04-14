d8918d6c2059771f02e7e4b5d57ff861918ee6f1
VladJu <juikovvlad@gmail.com>
2023-04-14 19:03:19 +0300

    Dagger 2 - ViewModelKey

--- app/src/main/java/com/example/startdependencyinjection/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/startdependencyinjection/example2/presentation/ViewModelFactory.kt
@@ -8,15 +8,13 @@ import javax.inject.Provider
 
 @ApplicationScope
 class ViewModelFactory @Inject constructor(
-    //JvmSuppressWildcards - надо чтобы даггер мог возвращать параметризованную коллекцию
-    private val viewModelsProviders :  @JvmSuppressWildcards Map<String, Provider<ViewModel>>
-) :ViewModelProvider.Factory {
+    private val viewModelsProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-       //тут из Мапки по имени класса поулчать VM и возвращать ее
-        //передаем имя класса и в случае ExampleViewModel  simpleName вернет название ExampleViewModel
+        //тут из Мапки по имени класса поулчаем VM и возвращать ее
         //при вызвое get()- каждый раз будет create new example VM
         // Providers - будет 1 и тот же но при вызвое get() будет возвращать новое значение
-        return viewModelsProviders[modelClass.simpleName]?.get() as T
+        return viewModelsProviders[modelClass]?.get() as T
     }
 }
